fe1e1d2a4e7486160b6b4ac1618c9e8915cd025b
Alexander Peltzer <alex.peltzer@gmail.com>
2019-12-16 15:38:20 +0100

    Add in params

--- bin/gProfileR.R
+++ bin/gProfileR.R
@@ -24,7 +24,7 @@ option_list = list(
   make_option(c("-d", "--model"), type="character", default=NULL, help="path to linear model file", metavar="character"),
   make_option(c("-n", "--normCounts"), type="character", default=NULL, help="path to normalized counts", metavar="character"),
   make_option(c("-s", "--species"), type="character", default=NULL, help="Species name. Example format: Hsapiens", metavar="character"),
-  make_option(c("-s", "--genelist"), type="character", default=NULL, help="path to genelist file", metavar="character")
+  make_option(c("-g", "--genelist"), type="character", default=NULL, help="path to genelist file", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list)
